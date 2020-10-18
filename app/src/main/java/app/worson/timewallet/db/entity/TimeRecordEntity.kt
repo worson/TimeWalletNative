66e1d421fa1f843d75263115f724426441470012
wangshengxing <www.sheng.xing@gmail.com>
2020-10-19 07:19:11 +0800

    add TimeRecordEntity.kt

---
+++ app/src/main/java/app/worson/timewallet/db/entity/TimeRecordEntity.kt
@@ -0,0 +1,26 @@
+package app.worson.timewallet.db.entity
+
+import androidx.annotation.Keep
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import org.jetbrains.annotations.NotNull
+
+/**
+ * 时间记录类型
+ */
+@Entity
+@Keep
+data class TimeRecordEntity (
+    @PrimaryKey(autoGenerate = true)
+    @NotNull
+    val id: Int = 0,
+    val uid: String,
+    val day: String,
+    val dayTimestamp: Long,
+    val distractCount: Int,
+    val endTime: Long,
+    val estimatedTime: Long,
+    val startTime: Long,
+    val thing: String?,
+    val typeId: Int,
+)
