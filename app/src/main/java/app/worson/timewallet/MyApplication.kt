7ce2d326d9108218e0d2618a026dbc5ebef6217f
wangshengxing <www.sheng.xing@gmail.com>
2020-10-27 18:24:14 +0800

    use AwesomeLog

--- app/src/main/java/app/worson/timewallet/MyApplication.kt
+++ app/src/main/java/app/worson/timewallet/MyApplication.kt
@@ -3,7 +3,10 @@ package app.worson.timewallet
 import android.app.Application
 import android.util.Log
 import app.worson.timewallet.db.TimeWalletDb
+import com.langogo.lib.log.internal.Platform
 import com.worson.lib.log.L
+import com.worson.lib.log.LogConfiguration
+import com.worson.lib.log.LogLevel
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -16,13 +19,24 @@ class MyApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        L.init(Log.DEBUG)
+        initConsolePrint(true)
         L.i(TAG, { "onCreate: " })
         GlobalScope.launch {
             L.i(TAG, "onCreate:start ")
             /*TimeWalletDb.instance.getEventEntityDao().addEvent(EventTypeEntity(uid = "0",name = "工作",desc = "赚钱",order = 1,color = 33))*/
 //            TimeWalletDb.instance.getEventEntityDao().queryById("0","3")
         }
+    }
+
+    fun initConsolePrint(debug:Boolean){
+        L.init(
+            LogConfiguration.Builder()
+                .logLevel(if (debug) LogLevel.ALL else LogLevel.DEBUG)
+                .threadInfo(debug)
+                .traceInfo(debug, 7)
+                .addPrinter(Platform.get().defaultPrinter())
+                .build()
+        )
     }
 
 }
